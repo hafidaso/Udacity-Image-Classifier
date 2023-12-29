2c2a4d6c42ca68505c324061cb08cecbc6bad0d5
Hafida Belayd <49368062+hafidaso@users.noreply.github.com>
2023-12-29 19:35:12 +0000

    run

--- functions_train.py
+++ functions_train.py
@@ -1,66 +1,42 @@
-# TODO: Build your network
-# We define a function in order to build the model. 
+# Function to construct a deep learning model based on specified architecture and hidden units
+def create_neural_network_model(architecture, num_hidden_units):
+    
+    import torch
+    from torch import nn
+    from torchvision import models
+    from collections import OrderedDict
 
-def build_dl_model(arch, hidden_units):
-    
-    import matplotlib.pyplot as plt 
-    import os 
-    import numpy as np
-    
-    import torch 
-    from torch import nn 
-    from torch import optim
-    import torch.nn.functional as F 
-    from torchvision import datasets, transforms, models   
-    import json
-    
-    # The output of the classifier, this value depends on the problem.
-    output_classifier = 102 
-    
-    if arch == "vgg19":
-        model = models.vgg19(pretrained=True)
-    elif arch == "resnet18":
-        model = models.resnet18(pretrained=True)
-    elif arch == "alexnet": 
-        model = models.alexnet(pretrained=True)
-    elif arch == "vgg16":
-        model = models.vgg16(pretrained=True) 
-    elif arch == "densenet161":
-        model = models.densenet161(pretrained=True) 
-    elif arch == "inception_v3": 
-        model = models.inception_v3(pretrained=True) 
-    else:
-        model = models.vgg19(pretrained=True)
-                   
-    # Freeze parameters 
-    for param in model.parameters():
-        param.requires_grad = False
+    # Define the number of output features for the classifier
+    num_output_features = 102 
 
-    from collections import OrderedDict 
+    # Load a pretrained model based on the specified architecture
+    available_models = {
+        "vgg19": models.vgg19,
+        "resnet18": models.resnet18,
+        "alexnet": models.alexnet,
+        "vgg16": models.vgg16,
+        "densenet161": models.densenet161,
+        "inception_v3": models.inception_v3
+    }
+    model = available_models.get(architecture, models.vgg19)(pretrained=True)
 
-    # I tried different architectures and hyperparameters but the 
-    # following works well. 
-    
-    # Getting the input size of the classifier
-    counter = 0 
-    input_classifer = 0 
+    # Disable training for all the parameters
+    for parameter in model.parameters():
+        parameter.requires_grad = False
 
-    for each in model.classifier.parameters():
-        if counter == 1:
-            break
-        
-        input_classifier = each.shape[1]
-        counter += 1
-    
+    # Determine the input size for the classifier
+    classifier_input_size = next(model.classifier.parameters()).shape[1]
 
+    # Define the classifier using an ordered dictionary
     classifier = nn.Sequential(OrderedDict([
-                                           ('fc1', nn.Linear(input_classifier, hidden_units)) , 
-                                           ('relu1', nn.ReLU()), 
-                                           ('dropout1', nn.Dropout(0.4)), 
-                                           ('fc2', nn.Linear(hidden_units, output_classifier)),
-                                           ('output', nn.LogSoftmax(dim=1))
-                                           ]))
+        ('fc1', nn.Linear(classifier_input_size, num_hidden_units)),
+        ('relu1', nn.ReLU()),
+        ('dropout1', nn.Dropout(0.4)),
+        ('fc2', nn.Linear(num_hidden_units, num_output_features)),
+        ('output', nn.LogSoftmax(dim=1))
+    ]))
 
-    model.classifier = classifier 
-    
+    # Replace the model's classifier
+    model.classifier = classifier
+
     return model
